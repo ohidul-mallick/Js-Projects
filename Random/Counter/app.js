39ac496060e101c85c642989762fca464cdedfc7
Ohidul Mallick <ohidulmallick999@gmail.com>
2021-03-21 20:14:01 +0530

    Random Project

---
+++ Random/Counter/app.js
@@ -0,0 +1,36 @@
+//Set Initial Count
+let count = 0;
+
+// Select value and Buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+btns.forEach(function(btn){
+btn.addEventListener('click',function(e){
+
+    // if (btn.innerText=='DECREASE'){
+    //     count--
+    // }
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')){
+        count--;
+    }else if(styles.contains('increase')){
+        count++;
+    }
+    else{
+        count = 0;
+    }
+    if(count>0){
+        value.style.color = 'green';
+    }else if(count<0){
+        value.style.color = 'red';
+    }else{
+        value.style.color ='black';
+    }
+
+
+
+
+    value.textContent = count;
+})
+});
